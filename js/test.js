22f18ff4d5525e3cd7e82b411f97775e4e88cd71
dshaw0331 <dylan.shaw@tealium.com>
2019-01-21 20:01:31 -0800

    reworked test file

--- js/test.js
+++ js/test.js
@@ -1,38 +1,62 @@
 
 if (tealiumTools.input && tealiumTools.input.csv_upload) {
-	debugger;
+	(function(){
+		// fetch current language prefs
+		var languages = utui.data.privacy_management.preferences.languages,
+			// fetch CSV data arr
+			data = tealiumTools.input.csv_upload.split(/\s/), 
+			// define expected header values --- change THESE if the headers in the UI change
+			actual_headers = ['language','title','category','confirmation_button','description','message','no','status','yes'],
+			chunk, 
+			rows = [],
+			i, j,
+			// method to convert array data to JSON data
+			toObj = function(data){
+			  var obj = {};
+			  for(i in input_headers){obj[input_headers[i]] = data[i];}
+			  return obj;
+			}, 
+			// method to validate input headers
+			validHeaders = function(input, actual){
+				var actual = actual, i, sameElements = true;
+				for(i in actual){
+					if(!input.includes(actual[i])){
+						sameElements = false; 
+						break;
+					}
+				}
+				return input.length == actual.length && sameElements;
+			}; 
+
+		// parse headers out of data arr
+		var input_headers = data.splice(0,9);
+
+		// check that headers are valid
+		if(!validHeaders(input_headers, actual_headers)){
+			alert('Please ensure that your first row contains all the expected header values.')
+			return;
+		}
+
+		// push remaining data into separate row arrays
+		while(data.length > 0){
+			chunk = data.splice(0,9);
+			rows.push(chunk);
+		}
+
+		// convert rows to JSON data
+		json_data = rows.map(function(row){return toObj(row);})
+
+
+		// overwrite language prefs with new data
+		for(j in json_data){
+		    languages[json_data[j].language].common_tokens = json_data[j];
+		}
+
+		console.log('THIS SHIT WORKS');
+		
+	})()
 } else {
-	alert('Please select a file from your machine.');
+	alert('Please paste some CSV data into the textbox.');
 }
-
-var languages = utui.data.privacy_management.preferences.languages,
-	data = tealiumTools.input.csv_upload.split(/\s/), 
-	chunk, 
-	rows = [],
-	i, j,
-	DataObj = function(data){
-	  var obj = {};
-	  for(i in headers){obj[headers[i]] = data[i];}
-	  return obj;
-	}; 
-
-// parse headers out
-var headers = data.splice(0,9);
-
-// push remaining data into separate row arrays
-while(data.length > 0){
-	chunk = data.splice(0,9);
-	rows.push(chunk);
-}
-
-// convert rows to JSON data
-json_data = rows.map(function(row){return DataObj(row);})
-
-
-// overwrite language prefs with new data
-for(j in json_data){
-    languages[json_data[j].language].common_tokens = json_data[j];
-}
-
 
 
