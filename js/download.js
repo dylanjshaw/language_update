f650215332f07b5fdd4fa6160ed5afd005acb7fa
dshaw0331 <dylan.shaw@tealium.com>
2019-01-22 16:29:05 -0800

    reworked both files

--- js/download.js
+++ js/download.js
@@ -1,78 +1,92 @@
-(function(){
-    var languages = utui.data.privacy_management.preferences.languages,
-        language_obj;
+(function() {
+    try {
+        var languages = utui.data.privacy_management.preferences.languages,
+            language_obj;
         items = [];
-    for (var i in languages) {
-        language_obj = languages[i].common_tokens;
-        language_obj.language = i;
-        items.push(language_obj);
-    }
+        for (var i in languages) {
+            language_obj = languages[i].common_tokens;
+            language_obj.language = i;
+            items.push(language_obj);
+        }
 
-    if (!items.length) {
-        tealiumTools.sendError('Error', 'Whoops! You do not have any language preferences defiend in TiQ.')
-    }
+        if (!items.length) {
+            tealiumTools.sendError('Error', 'Whoops! You do not have any language preferences defiend in TiQ.')
+        }
 
-    var headers = {
-            "language": "Language",
-            "title": "Title",
-            "category": "Category",
-            "confirmation_button": "Confirmation Button",
-            "description": "Description",
-            "message": "Message",
-            "no": "No",
-            "status": "Status",
-            "yes": "Yes"
-        },
-        fileTitle = 'tealium_language_preferences',
-        getDate = function(){
-            var date = new Date();
-            var dd = date.getDate();
-            var mm = date.getMonth()+1; 
-            var yyyy = date.getFullYear();
-            if(dd<10) {dd='0'+dd;} 
-            if(mm<10) {mm='0'+mm;} 
-            date = mm+'-'+dd+'-'+yyyy;
-            return date;
-        },
-        convertToCSV = function(headers, objArray) {
-            var objArray = objArray, str = '', line = '', i, j, line;
-            for (i in headers) {
-                if (line != '') {line += ',';}
-                line += headers[i];
-            }
-            str += line + '\r\n'; 
-            for (i in objArray) {
-                line = '';
-                for (j in Object.keys(headers)) {
-                    if (line != '') {line += ',';}
-                    line += objArray[i][Object.keys(headers)[j]];
+        var headers = {
+                "language": "Language",
+                "title": "Title",
+                "category": "Category",
+                "confirmation_button": "Confirmation Button",
+                "description": "Description",
+                "message": "Message",
+                "no": "No",
+                "status": "Status",
+                "yes": "Yes"
+            },
+            fileTitle = 'tealium_language_preferences',
+            getDate = function() {
+                var date = new Date();
+                var dd = date.getDate();
+                var mm = date.getMonth() + 1;
+                var yyyy = date.getFullYear();
+                if (dd < 10) {
+                    dd = '0' + dd;
+                }
+                if (mm < 10) {
+                    mm = '0' + mm;
+                }
+                date = mm + '-' + dd + '-' + yyyy;
+                return date;
+            },
+            convertToCSV = function(headers, objArray) {
+                var objArray = objArray,
+                    str = '',
+                    line = '',
+                    i, j, line;
+                for (i in headers) {
+                    if (line != '') {
+                        line += ',';
+                    }
+                    line += headers[i];
                 }
                 str += line + '\r\n';
-            }
-            return str;
-        },
-        exportCSVFile = function(headers, items, fileTitle) {
-            var csv = convertToCSV(headers, items);
-            var exportedFileName = fileTitle + '-' + getDate() + '.csv' || 'export.csv';
-            var blob = new Blob([csv], {
-                type: 'text/csv;charset=utf-8;'
-            });
-            if (navigator.msSaveBlob) {
-                navigator.msSaveBlob(blob, exportedFileName);
-            } else {
-                var link = document.createElement("a");
-                if (link.download !== undefined) {
-                    var url = URL.createObjectURL(blob);
-                    link.setAttribute("href", url);
-                    link.setAttribute("download", exportedFileName);
-                    link.style.visibility = 'hidden';
-                    document.body.appendChild(link);
-                    link.click();
-                    document.body.removeChild(link);
+                for (i in objArray) {
+                    line = '';
+                    for (j in Object.keys(headers)) {
+                        if (line != '') {
+                            line += ',';
+                        }
+                        line += objArray[i][Object.keys(headers)[j]];
+                    }
+                    str += line + '\r\n';
                 }
-            }
-            tealiumTools.sendMessage('Success', 'Good news! Everything worked.');
-        };
-
-    exportCSVFile(headers, items, fileTitle);
+                return str;
+            },
+            exportCSVFile = function(headers, items, fileTitle) {
+                var csv = convertToCSV(headers, items);
+                var exportedFileName = fileTitle + '-' + getDate() + '.csv' || 'export.csv';
+                var blob = new Blob([csv], {
+                    type: 'text/csv;charset=utf-8;'
+                });
+                if (navigator.msSaveBlob) {
+                    navigator.msSaveBlob(blob, exportedFileName);
+                } else {
+                    var link = document.createElement("a");
+                    if (link.download !== undefined) {
+                        var url = URL.createObjectURL(blob);
+                        link.setAttribute("href", url);
+                        link.setAttribute("download", exportedFileName);
+                        link.style.visibility = 'hidden';
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                    }
+                }
+                tealiumTools.sendMessage('Success', 'Good news! Everything worked.');
+            };
+        exportCSVFile(headers, items, fileTitle);
+    } catch (e) {
+        tealiumTools.sendError('Error', 'An unexpected error occurred. Please report this issue to Tealium Support.');
+    }
 })()
--- js/upload.js
+++ js/upload.js
@@ -1,85 +1,101 @@
 if (tealiumTools.input && tealiumTools.input.csv_upload) {
-	(function(){
-		// fetch current language prefs
-		var languages = utui.data.privacy_management.preferences.languages,
-			// fetch CSV data arr
-			data = tealiumTools.input.csv_upload.split(/\s/), 
-			// define expected header values --- change THESE if the headers in the UI change,
-			actual_header_map = {
-				'language':'Language',	
-				'title':'Title',	
-				'category':'Category',	
-				'confirmation_button':'Confirmation Button',	
-				'description':'Description',	
-				'message':'Message',	
-				'no':'No',	
-				'status':'Status',
-				'yes':'Yes'
-			},
-			chunk, 
-			rows = [],
-			i, j,
-			// method to convert array data to JSON data
-			toObj = function(data){
-			  var obj = {};
-			  for(i in Object.keys(actual_header_map)){obj[Object.keys(actual_header_map)[i]] = data[i];}
-			  return obj;
-			}, 
-			// method to validate input headers
-			validHeaders = function(input, actual){
-				var actual = actual, i, sameElements = true;
-				for(i in actual){
-					if(!input.includes(actual[i])){
-						sameElements = false; 
-						break;
-					}
-				}
-				return input.length == Object.keys(actual).length && sameElements;
-			}, 
-			undefined_languages = []; 
+    (function() {
+        try {
+            // fetch current language prefs
+            var languages = utui.data.privacy_management.preferences.languages,
+                // define expected header values --- change THESE if the headers in the UI change,
+                actual_header_map = {
+                    'language': 'Language',
+                    'title': 'Title',
+                    'category': 'Category',
+                    'confirmation_button': 'Confirmation Button',
+                    'description': 'Description',
+                    'message': 'Message',
+                    'no': 'No',
+                    'status': 'Status',
+                    'yes': 'Yes'
+                },
+                chunk,
+                rows = [],
+                i, j,
+                // method to convert array data to JSON data
+                toObj = function(data) {
+                    var obj = {};
+                    for (i in Object.keys(actual_header_map)) {
+                        obj[Object.keys(actual_header_map)[i]] = data[i];
+                    }
+                    return obj;
+                },
+                // method to validate input headers
+                validHeaders = function(input, actual) {
+                    var actual = actual,
+                        i, sameElements = true;
+                    for (i in actual) {
+                        if (!input.includes(actual[i])) {
+                            sameElements = false;
+                            break;
+                        }
+                    }
+                    return input.length == Object.keys(actual).length && sameElements;
+                },
+                removeWhiteSpaces = function(arr) {
+                    return arr.filter(function(str) {
+                        return str.replace(/\s/g, '')
+                    })
+                },
+                undefined_languages = [];
 
-		// parse headers out of data arr
-		var input_headers = data.splice(0,10);
+            // fetch CSV data arr
+            data = removeWhiteSpaces(tealiumTools.input.csv_upload.split(/\s/))
 
-		// process confirmation button header
-		var confirmation_index = input_headers.indexOf('Confirmation');
-		if(confirmation_index > -1){
-			if(input_headers[confirmation_index+1] === "Button"){
-				input_headers.splice(confirmation_index, 2);
-				input_headers.push('Confirmation Button')
-			}
-		}
+            // parse headers out of data arr
+            var input_headers = data.splice(0, 10)
+            debugger;
 
-		// check that headers are valid
-		if(!validHeaders(input_headers, actual_header_map)){
-			tealiumTools.sendError('Error','Please ensure that your first row contains all the expected header values.')
-			return;
-		}
+            // process confirmation button header
+            var confirmation_index = input_headers.indexOf('Confirmation');
+            if (confirmation_index > -1) {
+                if (input_headers[confirmation_index + 1] === "Button") {
+                    input_headers.splice(confirmation_index, 2);
+                    input_headers.push('Confirmation Button')
+                }
+            }
 
-		// push remaining data into separate row arrays
-		while(data.length > 0){
-			chunk = data.splice(0,9);
-			rows.push(chunk);
-		}
+            // check that headers are valid
+            if (!validHeaders(input_headers, actual_header_map)) {
+                tealiumTools.sendError('Error', 'Please ensure that your first row contains all the expected header values.')
+                return;
+            }
 
-		// convert rows to JSON data
-		json_data = rows.map(function(row){return toObj(row);})
+            // push remaining data into separate row arrays
+            while (data.length > 0) {
+                chunk = data.splice(0, 9);
+                rows.push(chunk);
+            }
 
-		// overwrite language prefs with new data
-		for(j in json_data){
-			if(languages[json_data[j].language]){
-		    	languages[json_data[j].language].common_tokens = json_data[j];
-			} else {
-				undefined_languages.push(json_data[j].language);
-			}
-		}
+            // convert rows to JSON data
+            json_data = rows.map(function(row) {
+                return toObj(row);
+            })
 
-		if(!undefined_languages.length){
-			tealiumTools.sendMessage('Success', 'Good news! Everything worked.');
-		} else {
-			tealiumTools.sendError('Error', 'The following languages were not updated because they were not previously defined in TiQ: ' + undefined_languages.toString());
-		}
-	})()
+            // overwrite language prefs with new data
+            for (j in json_data) {
+                if (languages[json_data[j].language]) {
+                    languages[json_data[j].language].common_tokens = json_data[j];
+                } else {
+                    undefined_languages.push(json_data[j].language);
+                }
+            }
+
+            if (!undefined_languages.length) {
+                tealiumTools.sendMessage('Success', 'Good news! Everything worked.');
+            } else {
+                tealiumTools.sendError('Error', 'The following languages were not updated because they were not previously defined in TiQ: ' + undefined_languages.toString());
+            }
+        } catch (e) {
+            tealiumTools.sendError('Error', 'An unexpected error occurred. Please report this issue to Tealium Support.')
+        }
+    })()
 } else {
-	tealiumTools.sendError('Error', 'Please paste some CSV data into the textbox.');
+    tealiumTools.sendError('Error', 'Please paste some CSV data into the textbox.');
 }
